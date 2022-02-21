4746bd109b925245fd775ef2286bf69c01fd0ea3
tomi <tomitsui123@gmail.com>
2022-02-21 13:00:06 +0800

    update create new record canvas

--- catcher-game/src/components/leaderboard/CreateNewRecordCanvas/index.tsx
+++ catcher-game/src/components/leaderboard/CreateNewRecordCanvas/index.tsx
@@ -1,12 +1,31 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
-import { Button } from 'react-bootstrap'
+import {
+  Alert,
+  Button,
+  Col,
+  Container,
+  Row,
+} from 'react-bootstrap'
 import { useNavigate, useParams } from 'react-router-dom'
 import { ILeaderboardEntity } from '../LeaderboardEntity'
+import gameBgImg from '../../../img/bg2.png'
+import styled from 'styled-components'
+import StyledButton from '../../styles/StyledButton'
+
+const StyledCanvas = styled.div`
+  padding-top: 100px;
+  background-image: url(${gameBgImg});
+  height: 100vh;
+  font-family: 'CursedTimer';
+  color: white;
+`
 
 const CreateNewRecordCanvas = () => {
   let navigate = useNavigate()
   const [data, setData] = useState<ILeaderboardEntity>()
+  const [name, setName] = useState<string>('')
+  const [error, setError] = useState<string>('')
   let { token } = useParams()
   useEffect(() => {
     getData()
@@ -15,18 +34,50 @@ const CreateNewRecordCanvas = () => {
     const res = await axios.get(`/api/${token}`)
     setData(res.data)
   }
-  const handleSubmit = () => {
-    navigate('/leaderboard')
+  const handleSubmit = async () => {
+    try {
+      const res = await axios.put(`/api/${token}`, null, {
+        params: { name },
+      })
+      console.log('hi')
+      if (res.data.message === 'OK') {
+        navigate(`/leaderboard/${token}`)
+      }
+    } catch (error) {
+      setError('not found')
+    }
   }
   return (
-    <div>
-      <h3>Your Score</h3>
-      <h1>5</h1>
-      <div>
-        <input />
-      </div>
-      <Button onClick={handleSubmit}>Submit</Button>
-    </div>
+    <StyledCanvas>
+      <Container>
+        <Row>
+          <Col md={4} sm={12} />
+          <Col md={4} style={{ textAlign: 'center' }}>
+            <h3>Your Score</h3>
+            <h1>{data?.count}</h1>
+            {error && (
+              <Alert style={{fontFamily: 'Arial'}} variant='danger'>
+                <Alert.Heading>
+                  Not Found
+                </Alert.Heading>
+              </Alert>
+            )}
+            <div>
+              <input
+                placeholder='Your name'
+                onChange={(e) =>
+                  setName(e.currentTarget.value)
+                }
+              />
+            </div>
+            <StyledButton onClick={handleSubmit}>
+              Submit
+            </StyledButton>
+          </Col>
+          <Col md={4} sm={12} />
+        </Row>
+      </Container>
+    </StyledCanvas>
   )
 }
 
