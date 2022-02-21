d9f2094e9b8dde1d8d205fb2ec176888fbc720f8
tomi <tomitsui123@gmail.com>
2022-02-21 13:00:48 +0800

    GamePage: add backend connection to the page

--- catcher-game/src/components/catcher/GamePage/index.tsx
+++ catcher-game/src/components/catcher/GamePage/index.tsx
@@ -1,22 +1,29 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import InfoBar from '../InfoBar'
 import { useInterval } from '../../../hooks/useInterval'
 import GameCanvas from './GameCanvas'
 import { Col, Container, Row } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import axios from 'axios'
 import {
   GAME_TIME,
   STAGE_HEIGHT,
   STAGE_WIDTH,
 } from '../../../constants'
 import StyledButton from '../../styles/StyledButton'
-import axios from 'axios'
 
 const GamePage = () => {
   const [gameOver, setGameOver] = useState(false)
   const [point, setPoint] = useState(0)
   const [token, setToken] = useState('')
-  const [remainingTime, setRemainingTime] = useState(GAME_TIME)
+  const [remainingTime, setRemainingTime] =
+    useState(GAME_TIME)
+
+const navigate = useNavigate()
   console.log('re-render')
+  useEffect(() => {
+    getToken()
+  }, [])
   useInterval(() => {
     if (remainingTime > 0) {
       setRemainingTime((prev) => prev - 1)
@@ -25,14 +32,24 @@ const GamePage = () => {
     }
   }, 1000)
   const startGame = async () => {
-    const res = await axios.post('/')
-    const { token } = res.data
-    setToken(token)
     setGameOver(false)
     setRemainingTime(GAME_TIME)
   }
 
-  const submitScore = () => {
+  const getToken = async () => {
+    const res = await axios.post('/api')
+    const { token } = res.data
+    setToken(token)
+  }
+
+  const submitScore = async () => {
+    const res = await axios.put(`/api/${token}`, null, {
+      params: { count: point },
+    })
+    const { message } = res.data
+    if (message === 'OK') {
+      navigate(`/leaderboard/new/${token}`)
+    }
   }
 
   return (
@@ -52,22 +69,42 @@ const GamePage = () => {
         >
           <Row>
             <Col>
-            <div>
-              <h3 style={{fontFamily: 'CursedTimer', color: 'white'}}>Your Score: </h3>
-              <h3 style={{fontFamily: 'CursedTimer', color: 'white'}}>{point}</h3>
-            </div>
+              <div>
+                <h3
+                  style={{
+                    fontFamily: 'CursedTimer',
+                    color: 'white',
+                  }}
+                >
+                  Your Score:{' '}
+                </h3>
+                <h3
+                  style={{
+                    fontFamily: 'CursedTimer',
+                    color: 'white',
+                  }}
+                >
+                  {point}
+                </h3>
+              </div>
             </Col>
           </Row>
           <Row>
             <Col>
-              <StyledButton onClick={() => submitScore()} style={{ fontSize: 14 }}>
+              <StyledButton
+                onClick={() => submitScore()}
+                style={{ fontSize: 14 }}
+              >
                 Send to leaderboard
               </StyledButton>
             </Col>
           </Row>
           <Row>
             <Col>
-              <StyledButton onClick={() => startGame()} style={{ fontSize: 14 }}>
+              <StyledButton
+                onClick={() => startGame()}
+                style={{ fontSize: 14 }}
+              >
                 Retry
               </StyledButton>
             </Col>
